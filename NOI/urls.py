bcf1f83025527cefbd2b3ab032350d1e68ea4bb0
yuan yong <446506536@qq.com>
2018-09-05 13:52:11 +0800

    index

--- NOI/urls.py
+++ NOI/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from  noi_app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('index/',views.index)
 ]
